ea4f1cb27ed2a54f1597fbafbb7ad3d439237d82
Akkshaj1-2099 <73285018+Akkshaj1-2099@users.noreply.github.com>
2020-10-30 15:07:49 +0530

    Add files via upload

---
+++ airbox.js
@@ -0,0 +1,3 @@
+function info1(){
+    window.alert("Sachin Ramesh Tendulkar is a former Indian international cricketer who was also the captain of the Indian national team. He is widely regarded as one of the greatest batsmen in the history of cricket. He is the highest run scorer of all time in International cricket.")
+}
